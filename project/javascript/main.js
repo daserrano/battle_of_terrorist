cc07e50b4e904a2427d6593c94ce4327eca828eb
David <david@david-VirtualBox.(none)>
2015-03-16 22:31:57 +0100

    arrayImage

--- project/javascript/main.js
+++ project/javascript/main.js
@@ -2,7 +2,34 @@ function game(){
     var c = document.getElementById("canvas");
     var ctx = c.getContext("2d");
 
-    drawWorld(ctx);
+
+    ctx.anchoCelda = 40;
+    ctx.altoCelda = 40;
+
+    ctx.arrayImagenes = new Array();
+
+    arrayImagenes[0] = new Image();
+    arrayImagenes[0].src = "../images/";
+
+    ctx.conjuntoTiles = [
+	new Tile(true, 1, ),
+	    new Tile(false, 2)
+		];
+
+    ctx.mapa = 
+	[
+	[1, 1, 1, 1, 1, 1, 1, 1],
+	[1, 0, 0, 0, 0, 0, 0, 1],
+	[1, 0, 0, 0, 0, 0, 0, 1],
+	[1, 1, 1, 1, 1, 1, 1, 1]
+	    ];
+
+    ctx.canvas.width = ctx.anchoCelda*ctx.mapa[0]length;
+    ctx.canvas.height = ctx.altoCelda*ctx.mapa.length;
+
+    ctx.jugador;
+
+    ctx.iniciarJugador();
 
 
 }
