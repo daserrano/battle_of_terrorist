cf1a74beac1b1375f18b48ca0cf8cb3f44888ac7
David <david@david-VirtualBox.(none)>
2015-03-21 19:30:33 +0100

    update

--- project/javascript/world.js
+++ project/javascript/world.js
@@ -4,11 +4,11 @@ function world(idCanvas, button)
     this.context = this.canvas.getContext('2d');
 
     this.cellWidth = 60;
-    this.cellHeigth = 60;
+    this.cellHeight = 60;
 
     this.tiles = [
-	new tile(this.cellWidth, this.cellHeigth, true, "white"),
-	new tile(this.cellWith, this.cellHeigth, false, "blue") 
+	new tile(this.cellWidth, this.cellHeight, true, "white"),
+	new tile(this.cellWith, this.cellHeight, false, "blue") 
 		];
 
     this.map = 
@@ -19,4 +19,27 @@ function world(idCanvas, button)
     [0, 1, 1, 1, 0, 1, 1, 0],
     [0, 0, 0, 0, 0, 0, 0, 0]
 	];
+
+    this.canvas.width = this.cellWidth*this.mapa[0].length;
+    this.canvas.height = this.cellHeight*this.mapa.length;
+
+    
+    Mundo.prototype.casillaCaminable = function(px, py)
+    {
+	var x = parseInt(px);
+	var y = parseInt(py);
+
+	return this.tiles[this.map[x][y]].caminable;
+    }
+
+
+    Mundo.prototype.dibujarMapa = function()
+    {
+	var y = this.mapa.length;
+	var x = this.mapa[0].length;
+
+	for (var yi=0; yi < y; yi++)
+	    for(var xi=0, xi < x; xi++)
+		this.tiles[this.map[yi][xi]].dibujar(this.context, xi, yi);
+    }
 }
