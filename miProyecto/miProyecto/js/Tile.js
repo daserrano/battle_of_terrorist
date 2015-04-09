1f1d6ffadb203b7cbe0ee67ab02a6f1e653fa3dd
danieltorreslebruno <danieltorreslebruno@gmail.com>
2015-04-09 21:37:36 +0200

    miProject
    Uploading tests of miProjecto

---
+++ miProyecto/miProyecto/js/Tile.js
@@ -0,0 +1,17 @@
+/**********************************/
+/* Juan Gabriel Rodríguez Carrión */
+/*    jlabstudio.com       2012   */
+/**********************************/
+function Tile(ancho, alto, caminable, color){
+	this.ancho=ancho;
+	this.alto=alto;
+	this.caminable=caminable;
+	this.color=color;
+}
+Tile.prototype.dibujar=function(contexto,x,y){
+	contexto.fillStyle = "#444";
+	contexto.fillRect(this.ancho*x,this.alto*y,this.ancho,this.alto);
+	contexto.fillStyle = this.color;
+	contexto.fillRect(this.ancho*x+1,this.alto*y+1,this.ancho-2,this.alto-2);
+};
+	
