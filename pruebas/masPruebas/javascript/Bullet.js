70d01995692ae14d2c743e0c380c85403dc539ad
daserrano <sein2809@gmail.com>
2015-04-24 14:53:37 +0200

    collision class

--- pruebas/masPruebas/javascript/Bullet.js
+++ pruebas/masPruebas/javascript/Bullet.js
@@ -2,34 +2,44 @@ function Bullet(world, player)
 {
 	this.width  = 10; // Tamaño de la bala.
 	this.height = 10;
-	this.velocity  = 0.5; // Velocidad de la bala.
+	this.velocity  = 0.025; // Velocidad de la bala.
 	this.direction = player.direction; // Direccion.
+	this.px = world.player.x; // Posición de la bala.
+	this.py = world.player.y;
 
-	this.px = player.x*world.cellWidth; // Posición de la bala.
-	this.py = player.y*world.cellHeight;
+	//alert(world.player.x + " " + world.player.y + " " + this.px + " " + this.py);
 
-	if(this.direction == "right" || this.direction == "left")
-		this.py -=5;
+/*	if(this.direction == "right" || this.direction == "left")
+		this.py -= 5;
 	if(this.direction == "left")
-		this.px -=5;
+		this.px -= 5;
 	if(this.direction == "down")
-		this.px -=15;
+		this.px -= 15;
 	if(this.direction == "up")
-		this.py -=5;
+		this.py -= 20;*/
 
 }
 
 Bullet.prototype.move = function(delta)
 {
-	
+	var colX = new Collision(this.px, world.player.y);
+	var colY = new Collision(world.player.x, this.py);
+
+	if(!colX.validPosition(this, this.px, world.player.y))
+		//px = this.x;
+	if(!colY.validPosition(this, world.player.x, this.py))
+		//py = this.y;
+alert(this.py + " " + this.px);	
 	if(this.direction == "up")
-		this.py -= 1*this.velocity*delta;
+		this.py -= this.velocity*delta;
 	else if(this.direction == "down")
-		this.py += 1*this.velocity*delta;
+		this.py += this.velocity*delta;
 	else if(this.direction == "right")
-		this.px += 1*this.velocity*delta;
+		this.px += this.velocity*delta;
 	else if(this.direction == "left")
-		this.px -= 1*this.velocity*delta;
+		this.px -= this.velocity*delta;
+
+	//alert("px: " + px + " py: " + py);
 
 };
 
@@ -43,9 +53,9 @@ Bullet.prototype.draw = function(context)
 	//t.translate(this.x*world.cellWidth, this.y*world.cellHeight);
 
 	//world.sprite.drawBullet(context, this.width, this.height, this.direction);
+		context.translate(this.px*world.cellWidth, this.py*world.cellHeight);
 		context.fillRect(this.px, this.py, this.width, this.height);
 		
-
 	context.restore();
 }
 
--- pruebas/masPruebas/javascript/Character.js
+++ pruebas/masPruebas/javascript/Character.js
@@ -20,10 +20,10 @@ function Character(world, width, height, x, y, sprite)
 	this.transition       =  0; // Tiempo que transcurre.
 }
 
-Character.prototype.validPosition = function(px, py)
+/*Character.prototype.validPosition = function(px, py)
 {
-	var width  = this.width/(2*world.cellWidth);
-	var height = this.height/(2*world.cellHeight);
+	var width  = this.width/(3*world.cellWidth); //0.4
+	var height = this.height/(3*world.cellHeight);
 
 	if(!world.cellWalked(px-width, py-height))
 		return false;
@@ -35,7 +35,7 @@ Character.prototype.validPosition = function(px, py)
 		return false;
 
 	return true;
-};
+};*/
 
 Character.prototype.move = function(delta)
 {
@@ -46,9 +46,12 @@ Character.prototype.move = function(delta)
 	var px = this.x+this.dx*this.velocity*delta;
 	var py = this.y+this.dy*this.velocity*delta;
 
-	if(!this.validPosition(px, this.y)) //Comprobar si es una posicion valida.
+	var colX = new Collision(px, this.y);
+	var colY = new Collision(this.x, py);
+
+	if(!colX.validPosition(this, px, this.y))
 		px = this.x;
-	if(!this.validPosition(this.x, py))
+	if(!colY.validPosition(this, this.x, py))
 		py = this.y;
 
 	if(this.x==px && this.y==py) //Si no hay movimiento.
