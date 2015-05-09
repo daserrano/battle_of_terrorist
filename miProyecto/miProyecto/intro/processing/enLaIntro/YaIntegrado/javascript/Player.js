3ede1133d19b53fcfba84cba0b357fc34758b7de
danieltorreslebruno <danieltorreslebruno@gmail.com>
2015-05-09 23:43:38 +0200

    Integrado Processing intro

---
+++ miProyecto/miProyecto/intro/processing/enLaIntro/YaIntegrado/javascript/Player.js
@@ -0,0 +1,28 @@
+function Player(world, width, height, x, y, id)
+{
+	Character.call(this, world, width, height, x, y, Sprites.get(id)); //Llama al método personaje para que tenga los mismos atributos.
+	this.up    = false;
+	this.down  = false;
+	this.right = false;
+	this.left  = false;
+	this.shoot = false;
+} 
+
+Player.prototype = new Character;
+
+Player.prototype.move = function(delta, other)
+{
+	this.dx = 0;
+	this.dy = 0;
+
+	if(this.up)
+		this.dy -= 1;
+	if(this.down)
+		this.dy += 1;
+	if(this.right)
+		this.dx += 1;
+	if(this.left)
+		this.dx -= 1;
+
+	Character.prototype.move.call(this, delta, other);
+};
