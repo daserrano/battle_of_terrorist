89fe0d294ec660caa87c517aa69bc78d8860c40b
danieltorreslebruno <danieltorreslebruno@gmail.com>
2015-05-10 19:46:03 +0200

    trying choose players

--- miProyecto/miProyecto/intro/processing/enLaIntro/YaIntegrado/javascript/processingFunction.js
+++ miProyecto/miProyecto/intro/processing/enLaIntro/YaIntegrado/javascript/processingFunction.js
@@ -6,10 +6,12 @@ function sketchProc(processing) {
   var drag = 1;
   var can = false;
   var rectX1, rectY1, rectX2, rectY2, rectX3, rectY3, rectX4, rectY4, rectJUGARX, rectJUGARY;
-  var rectOver = false, rectOver1 = false, rectOver2 = false, rectOver3 = false, rectOver4 = false, texto = false;
+  var rectOver = false, rectOver1 = false, rectOver2 = false, rectOver3 = false, rectOver4 = false,rectOver5 = false,rectOver6 = false,rectOver7 = false, texto = false;
   var transformer;
-  var hapasado;
-
+  var crearRectangulos = false;
+  var mira = false;
+  var personaje;
+  var color;
 
       //SETUP Processing
       processing.size(1250, 650);
@@ -24,6 +26,8 @@ function sketchProc(processing) {
       tercerPersonaje = processing.loadImage("img/3FIN.png");
       cuartoPersonaje = processing.loadImage("img/4FIN.png");
       MOD = processing.loadImage("img/ratonMOD.png");  
+
+
       logo = processing.loadImage("img/logo1.png")
       processing.textFont(transformer, 65);
 
@@ -35,6 +39,12 @@ function sketchProc(processing) {
       rectY3 = 430;
       rectX4 = 970;
       rectY4 = 430;
+      rectX5 = 230;
+      rectX6 = 518;
+      rectX7 = 806;
+      rectY5 = 550;
+      rectY6 = 550;
+      rectY7 = 550;
       rectJUGARX = 325; 
       rectJUGARY = 550; 
 
@@ -42,15 +52,15 @@ function sketchProc(processing) {
   //DRAW
   processing.draw = function() { 
     processing.background(0);
-    processing.noCursor();
     processing.text("Elige un personaje", 310, 310);
 
-    processing.image(MOD, xpos, ypos);
     processing.image(logo, 475, 25, 300, 150, 7);
     processing.image(primerPersonaje, 185, 350,50,70);
     processing.image(segundoPersonaje, 465, 350, 50, 70);
     processing.image(tercerPersonaje, 740, 350, 50, 70);
     processing.image(cuartoPersonaje, 1020, 350, 50, 70);
+    processing.noCursor();
+    processing.image(MOD, xpos, ypos);
 
     var difx = processing.mouseX - xpos-MOD.width/2;
     if(processing.abs(difx) > 1.0) {
@@ -99,15 +109,47 @@ function sketchProc(processing) {
       processing.text("BILLY", 980, 475);
       processing.noFill();
     } 
-
-    if(rectOver1 == false && rectOver2 == false && rectOver3 == false && rectOver4 == false){
-      processing.fill(204, 204, 0);
-    }
-if(hapasado == 1){
-    processing.rect(600, 550, 100, 50, 10);
-
-
+    if(mira == true){
+      if (rectOver5){
+        processing.text("  ?  ", 125, 475);
+        processing.text("  ?  ", 405, 475);
+        processing.text("  ?  ", 685, 475);
+        processing.text("  ?  ", 980, 475);
+        processing.fill(235, 0, 0);
+        processing.text(" ROJO ", 240, 595);
+        processing.noFill();
+      } 
+      if (rectOver6){
+        processing.text("  ?  ", 125, 475);
+        processing.text("  ?  ", 405, 475);
+        processing.text("  ?  ", 685, 475);
+        processing.text("  ?  ", 980, 475);
+        processing.fill(0, 0, 235);
+        processing.text(" AZUL ", 521, 595);
+        processing.noFill();
+      } 
+      if (rectOver7){
+        processing.text("  ?  ", 125, 475);
+        processing.text("  ?  ", 405, 475);
+        processing.text("  ?  ", 685, 475);
+        processing.text("  ?  ", 980, 475);
+        processing.fill(0, 210, 0);
+        processing.text("VERDE ", 820, 595);
+        processing.noFill();
+      } 
 }
+      if(rectOver1 == false && rectOver2 == false && rectOver3 == false && rectOver4 == false && rectOver5 == false && rectOver6 == false && rectOver7 == false){
+        processing.fill(204, 204, 0);
+      }
+      if(crearRectangulos == true){
+    //processing.fill(255, 0, 0);
+    mira = true;
+    processing.rect(rectX5, rectY5, 213, 50, 22);
+    processing.rect(rectX6, rectY6, 213, 50, 22);
+    processing.rect(rectX7, rectY7, 213, 50, 22);
+
+
+  }
 
 
 
@@ -130,12 +172,24 @@ if(hapasado == 1){
     else if(overRect(rectX4, rectY4, 160, 50)){
       rectOver4 = true;
     }
+    else if(overRect(rectX5, rectY5, 213, 50)){
+      rectOver5 = true;
+    }
+    else if(overRect(rectX6, rectY6, 213, 50)){
+      rectOver6 = true;
+    }
+    else if(overRect(rectX7, rectY7, 213, 50)){
+      rectOver7 = true;
+    }
     if( overRect(rectX1, rectY1, 160, 50) == false && 
       overRect(rectX2, rectY2, 160, 50) == false && 
       overRect(rectX3, rectY3, 160, 50) == false && 
-      overRect(rectX4, rectY4, 160, 50) == false){
+      overRect(rectX4, rectY4, 160, 50) == false &&
+      overRect(rectX5, rectY5, 213, 50) == false &&
+      overRect(rectX6, rectY6, 213, 50) == false &&
+      overRect(rectX7, rectY7, 213, 50) == false ){
 
-      rectOver1 = rectOver2 = rectOver3 = rectOver4 = false;
+      rectOver1 = rectOver2 = rectOver3 = rectOver4 = rectOver5 = rectOver6 = rectOver7 = false;
   }
 } //update
 
@@ -143,11 +197,14 @@ if(hapasado == 1){
 processing.mousePressed = function() {
 
   if (rectOver1) {
+    crearRectangulos = true;
+    processing.personaje = 1;
     processing.exit();
     processing.noLoop();
-    can = true;
-    canvas.width = canvas.width;
+    dime();
     prueba();
+    //canvas.width = canvas.width;
+    //prueba();
     //World(canvas);
   }
   if (rectOver2) {
@@ -163,6 +220,23 @@ processing.mousePressed = function() {
     //World(canvas);
   }
   if (rectOver4) {
+    processing.exit();
+    processing.noLoop();
+    //World(canvas);
+  }
+   if (rectOver5) {
+    processing.exit();
+    processing.noLoop();
+    //World(canvas);
+  }
+   if (rectOver6) {
+    color = rojo;
+    processing.exit();
+    processing.noLoop();
+    seleccion(personaje, color);
+    //World(canvas);
+  }
+   if (rectOver7) {
     processing.exit();
     processing.noLoop();
     //World(canvas);
@@ -184,12 +258,13 @@ else {
 }
 }
 
-  var canvas
-  var sketch
-  var p
+var canvas
+var sketch
+var p
 function cargarCanvas(){
-canvas = document.getElementById("canvas1");
-p = new Processing(canvas, sketchProc);
-sketch = p.externals.sketch; 
-sketch.options.crispLines = true;
+  canvas = document.getElementById("canvas1");
+  p = new Processing(canvas, sketchProc);
+  sketch = p.externals.sketch; 
+  sketch.options.crispLines = true;
 }
+
